6931ba62d5926b21e5dd21730725a3372e34821d
yura <yricog1290@gmail.com>
2024-09-28 02:16:49 +0900

    Router,Graphql,Apollo 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://54.180.61.91:5000/graphql/",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,66 @@
+// import { gql, useQuery } from "@apollo/client";
+
+// const GET_KAKAO_AUTH_URL = gql`
+//   query GetKakaoAuthUrl {
+//     getKakaoAuthUrl {
+//       url
+//     }
+//   }
+// `;
+
+function Home() {
+  //   const { loading, error, data } = useQuery(GET_KAKAO_AUTH_URL);
+
+  //   const handleKakaoLogin = () => {
+  //     if (data && data.getKakaoAuthUrl) {
+  //       window.location.href = data.getKakaoAuthUrl.url;
+  //     }
+  //   };
+
+  //   if (loading) return <p>Loading...</p>;
+  //   if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            TODOKKAEBI
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            투두깨비에 오신 것을 환영합니다!😄
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                // onClick={handleKakaoLogin}
+              >
+                KAKAO LOGIN
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
