c3df4752b649ba71efaa3dd793e94b4dde9abe4b
yura <yricog1290@gmail.com>
2024-09-30 13:17:08 +0900

    배포 경로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,74 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    colors: {
+      primary: '#ff7849',
+      secondary: '#ffc82c',
+      white: '#ffffff',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+      },
+    },
+    extend: {
+      spacing: {
+        13: '3.25rem', // p-13, m-13, h-13
+        15: '3.75rem',
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600',
+        },
+        '.btn-blue': {
+          backgroundColor: '#3490dc',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#2779bd',
+          },
+        },
+        '.btn-red': {
+          backgroundColor: '#e3342f',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a',
+          },
+        },
+      });
+    }),
+  ],
+  important: true,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "development" ? "/" : "./",
+  // base: process.env.NODE_ENV === "development" ? "/" : "./",
   server: {
     port: 3000, // 서버 포트 번호 변경
   },
   define: {
-    global: "{}", // 글로벌 객체를 빈 객체로 대체
+    global: '{}', // 글로벌 객체를 빈 객체로 대체
   },
   build: {
-    outDir: "build", // build 폴더명 변경
+    outDir: 'build', // build 폴더명 변경
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         },
       },
@@ -31,10 +27,10 @@ export default defineConfig({
   },
   resolve: {
     alias: [
-      { find: "@", replacement: resolve(__dirname, "./src") },
+      { find: '@', replacement: resolve(__dirname, './src') },
       {
-        find: "@styles",
-        replacement: resolve(__dirname, "./src/assets/scss"),
+        find: '@styles',
+        replacement: resolve(__dirname, './src/assets/scss'),
       },
     ],
   },
